a0d14d3d7ac033e76da635c305af44cfb24aa774
MauroJRamos <mauroramosti@gmail.com>
2021-10-09 16:45:40 -0300

    Create controllers, repositories e services

---
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -0,0 +1,42 @@
+package com.devsuperior.bds02.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.repositories.CityRepository;
+
+@Service
+public class CityService {
+	
+	@Autowired
+	private CityRepository repository;
+	
+	public List<CityDTO> findAll(){
+		List<City> list = repository.findAll(Sort.by("name"));
+		return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public CityDTO insert(CityDTO dto) {
+		City entity = new City();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new CityDTO(entity);
+	}
+
+	private void copyDtoToEntity(CityDTO dto, City entity) {
+		
+		entity.setName(dto.getName());
+	
+		
+	}
+	
+
+}
